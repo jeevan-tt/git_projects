5229caaa17c945f2a522ebd3cf1d3fd70b2b6b14
jeevan tt <55012835+jeevan-tt@users.noreply.github.com>
2023-02-07 01:29:54 +0530

    Handling of security
    We have checking the password which has given to specific file

--- ListToExcelConversionproject/src/main/java/Excel/project/ReadSheetPoi.java
+++ ListToExcelConversionproject/src/main/java/Excel/project/ReadSheetPoi.java
@@ -3,38 +3,50 @@ package Excel.project;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-import javax.print.DocFlavor.STRING;
-
+import org.apache.poi.poifs.crypt.EncryptionInfo;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.xssf.usermodel.*;
+
 public class ReadSheetPoi {
 
 	public static void main(String[] args) throws IOException {
 
-		String exceLPath = ".\\data\\Contactslist.xlsx";
+		String exceLPath = "C:\\Users\\jeeva\\OneDrive\\Desktop\\jeevan.xlsx";
 		FileInputStream fileInputStream = new FileInputStream(exceLPath);
+		String password = "";
 
-		
 		XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
-		XSSFSheet sheet = workbook.getSheet("Cobtacts");
-		
-		int rows = sheet.getLastRowNum();
-		int cols = sheet.getRow(1).getLastCellNum();
-		for (int r = 0; r <= rows; r++) {
-			XSSFRow row = sheet.getRow(r);
-			for (int c = 0; c <= cols; c++) {
-				XSSFCell cell = row.getCell(c);
-				cell.getBooleanCellValue();
-				
-				System.out.println();
+		XSSFSheet sheet = null;
 
+		sheet = workbook.getSheet("Contacts");
+
+		if (null != sheet && sheet.getProtect()) {
+
+			if (sheet.validateSheetPassword(password)) {
+
+				int rows = sheet.getLastRowNum();
+				int cols = sheet.getRow(1).getLastCellNum();
+				for (int r = 0; r < rows; r++) {
+					XSSFRow row = sheet.getRow(r);
+					for (int c = 0; c < cols; c++) {
+						XSSFCell cell = row.getCell(c);
+
+						System.out.print(cell.getStringCellValue() + "\t");
+
+					}
+					System.out.println();
+
+				}
+			} else {
+				System.out.println("File Mismatch");
 			}
 
+		} else {
+			System.out.println("UN AUTHERISED");
 		}
-
 	}
 
 }
